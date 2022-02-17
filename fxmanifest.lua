f2f24434e0a4608b7cbbd2bdaf294d3bbce40dc7
Twice4sNice <93999066+Twice4sNice@users.noreply.github.com>
2022-02-17 21:15:10 +0100

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'A script to change your character into a ped'
+version '1.0'
 
-version '1.0.1'
-
-server_script 'server.lua'
-
-client_script 'client.lua'
+server_script 'server/server.lua'
+client_script 'client/client.lua'
 
 dependencies {
 	'tprp_base'
